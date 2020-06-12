f48fe096035f94af95c39f8d7d8904a3227e08ca
mihirKachroo <60433268+mihirKachroo@users.noreply.github.com>
2020-06-12 11:41:33 -0400

    useless commit

--- lib/getRequest.dart
+++ lib/getRequest.dart
@@ -1,14 +1,8 @@
 import 'package:http/http.dart';
 
-void main() {
-  _makeGetRequest();
-}
-
-_makeGetRequest() async {
-  // make GET request
+_doTransaction() async {
   String url = 'https://uploaded-report-api.herokuapp.com/';
   Response response = await get(url);
-  // sample info available in response
   int statusCode = response.statusCode;
   Map<String, String> headers = response.headers;
   String contentType = headers['content-type'];
