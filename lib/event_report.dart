457130c80951f890612e71c8a7793508c6376575
AgrawalTrisha <agrawaltrisha@gmail.com>
2020-06-12 10:37:54 -0500

    made camera and congrats page

--- lib/event_report.dart
+++ lib/event_report.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:report/transaction_completed.dart';
 
 class EventReport extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.green,
       body: Container(
-        color: Colors.white,
-        child:
+        alignment: Alignment.bottomCenter,
+        child: IconButton(
+          iconSize: 50,
+          icon: Icon(Icons.camera_alt),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => TransactionCompleted()),
+            );
+          },
+        )
       ),
     );
   }
